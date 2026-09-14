edd60b23db4fc3ce5f56eb9bc5cc9f3602fe723d
M-Ulmann <marek.ulmannek@gmail.com>
2026-09-14 16:40:31 +0200

    Pridane HTML do root stranky

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,33 @@
+import Link from "next/link";
+
 export default function Home(){
   return(
-    <p>cs ahoj jak je</p>
+    <>
+      <header>
+        <Link href={'/'}>
+          Home        
+        </Link>
+
+        <Link href={'/pokemon-generator'}>
+          Pokemon Generator
+        </Link>
+
+        <Link href={'/crypto-dashboard'}>
+          Crypto Dashboard
+        </Link>
+      </header>
+
+      <main>
+        <h1>Pick an API project</h1>
+        
+        <Link href={'/pokemon-generator'}>
+          obrazek here
+        </Link>
+
+        <Link href={'/crypto-dashboard'}>
+          obrazek here
+        </Link>
+      </main>
+    </>
   );
 }

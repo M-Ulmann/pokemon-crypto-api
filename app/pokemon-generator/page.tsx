b81f9e6c73303839744784370b62cb99aeb18f5f
M-Ulmann <marek.ulmannek@gmail.com>
2026-09-15 14:48:52 +0200

    Pridane HTML v pokemon-generator

--- app/pokemon-generator/page.tsx
+++ app/pokemon-generator/page.tsx
@@ -3,6 +3,8 @@
 import Header from "@/components/header";
 import { useState } from "react";
 import clsx from "clsx";
+import Image from "next/image";
+import Pikachu from "@/public/pikachu.png"
 
 export default function PokemonGenerator(){
 
@@ -12,7 +14,62 @@ export default function PokemonGenerator(){
     <div className={clsx("transition", isDark ? "bg-slate-800 text-white" : "bg-white text-black")}>
       
       <Header isDark={isDark} setIsDark={setIsDark}/>
-      <p>pokemoni</p>
+      
+      <main>
+
+        <section>   
+
+          <div></div>
+
+          <p>
+            <span>hp</span> 46        
+          </p>
+
+          <Image
+            src={Pikachu}
+            width={100}
+            height={100}
+            alt="pika pika"
+          />
+
+          <p>
+            pikachu
+          </p>
+          
+          <div>
+
+            <p>poison</p>
+
+            <p>normal</p>
+
+          </div>
+
+          <div>
+
+            <div>
+              <p>70</p>
+              <p>Attack</p>
+            </div>
+
+            <div>
+              <p>120</p>
+              <p>defense</p>
+            </div>
+
+            <div>
+              <p>52</p>
+              <p>speed</p>
+            </div>
+
+          </div>
+
+        </section>
+
+        <button>
+          Generate
+        </button>
+
+      </main>
 
     </div>
   );

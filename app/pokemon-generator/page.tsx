75ebefd43c2fadff547bd3bb85e4eae4de51b456
M-Ulmann <marek.ulmannek@gmail.com>
2026-09-15 15:41:20 +0200

    Pridane CSS v pokemon-generator

--- app/pokemon-generator/page.tsx
+++ app/pokemon-generator/page.tsx
@@ -15,49 +15,50 @@ export default function PokemonGenerator(){
       
       <Header isDark={isDark} setIsDark={setIsDark}/>
       
-      <main>
+      <main className="flex flex-col items-center mt-10 gap-10">
 
-        <section>   
+        <section className="flex flex-col items-center overflow-hidden min-w-125 max-w-125 relative px-5 py-10 gap-7 rounded-2xl shadow-[2px_5px_10px_5px_rgba(0,0,0,0.25)]">   
 
-          <div></div>
+          <div className="absolute bg-yellow-500 w-130 h-120 -top-50 rounded-full z-0"></div>
 
-          <p>
-            <span>hp</span> 46        
+          <p className="bg-white rounded-4xl self-end px-6 py-2 text-xl z-10 text-black">
+            <span className=" uppercase text-sm font-bold">hp</span> 46        
           </p>
 
           <Image
             src={Pikachu}
-            width={100}
-            height={100}
+            width={250}
+            height={250}
             alt="pika pika"
+            className="z-10"
           />
 
-          <p>
+          <p className=" capitalize text-4xl text-center font-bold">
             pikachu
           </p>
           
-          <div>
+          <div className="flex flex-row flex-wrap gap-20 font-bold text-white">
 
-            <p>poison</p>
+            <p className="bg-yellow-500 rounded-3xl px-5 py-1">poison</p>
 
-            <p>normal</p>
+            <p className="bg-yellow-500 rounded-3xl px-5 py-1">normal</p>
 
           </div>
 
-          <div>
+          <div className="flex flex-row gap-20 text-xl tracking-[2px]">
 
-            <div>
-              <p>70</p>
+            <div className="flex flex-col items-center">
+              <p className="font-bold text-2xl text-yellow-500">70</p>
               <p>Attack</p>
             </div>
 
-            <div>
-              <p>120</p>
+            <div className="flex flex-col items-center">
+              <p className="font-bold text-2xl text-yellow-500">120</p>
               <p>defense</p>
             </div>
 
-            <div>
-              <p>52</p>
+            <div className="flex flex-col items-center">
+              <p className="font-bold text-2xl text-yellow-500">52</p>
               <p>speed</p>
             </div>
 
@@ -65,7 +66,7 @@ export default function PokemonGenerator(){
 
         </section>
 
-        <button>
+        <button className="bg-black text-white outline-4 outline-black px-10 py-5 text-3xl rounded-lg hover:bg-white hover:text-black active:bg-gray-300 transition duration-300">
           Generate
         </button>
 

f7a1edba9532813d5fd56cf1563182de09333a38
M-Ulmann <marek.ulmannek@gmail.com>
2026-09-15 14:31:05 +0200

    Setup stranek na budouci pouziti

--- app/crypto-dashboard/page.tsx
+++ app/crypto-dashboard/page.tsx
@@ -1,10 +1,20 @@
+"use client"
+
 import Header from "@/components/header";
+import { useState } from "react";
+import clsx from "clsx";
 
 export default function CryptoDashboard(){
+
+  const [isDark, setIsDark] = useState(false);
+
   return (
-    <>
-      <Header/>
-      <p>crypto</p>
-    </>
+    <div className={clsx("transition", isDark ? "bg-slate-800 text-white" : "bg-white text-black")}>
+      <Header isDark={isDark} setIsDark={setIsDark}/>
+      
+      <main>
+
+      </main>
+    </div>
   );
 }
--- app/pokemon-generator/page.tsx
+++ app/pokemon-generator/page.tsx
@@ -1,10 +1,19 @@
+"use client"
+
 import Header from "@/components/header";
+import { useState } from "react";
+import clsx from "clsx";
 
 export default function PokemonGenerator(){
+
+  const [isDark, setIsDark] = useState(false);
+
   return(
-    <>
-      <Header />
+    <div className={clsx("transition", isDark ? "bg-slate-800 text-white" : "bg-white text-black")}>
+      
+      <Header isDark={isDark} setIsDark={setIsDark}/>
       <p>pokemoni</p>
-    </>
+
+    </div>
   );
 }

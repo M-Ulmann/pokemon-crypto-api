54370fb493bb5e4dd1631d0e25e4167a128619af
M-Ulmann <marek.ulmannek@gmail.com>
2026-09-15 13:46:42 +0200

    Pridany dark mode na root strance

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,32 @@
+"use client"
+
 import Link from "next/link";
 import Image from "next/image";
 import Pikachu from "@/public/pikachu.png"
 import Bitcoin from "@/public/bitcoin.png"
 import Header from "@/components/header";
+import { useState } from "react";
+import clsx from "clsx";
 
 export default function Home(){
+
+  const [isDark, setIsDark] = useState(false);
+
   return(
-    <>
+    <div className={clsx("transition", isDark ? "bg-slate-800 text-white" : "bg-white text-black")}>
 
-      <Header />
+      <Header isDark={isDark} setIsDark={setIsDark} />
 
-      <main className="flex flex-col items-center justify-start gap-10 mt-20">
+      <main className="flex flex-col items-center justify-start gap-10 mt-20 min-h-[90dvh]">
         <h1 className="text-5xl font-bold tracking-[8px] text-center px-3">
           Pick an API project
         </h1>
         
         <div className="grid grid-cols-2 gap-10 text-center max-w-11/12 sm:max-w-1/2">
           <Link href={'/pokemon-generator'}
-            className="rounded-2xl hover:shadow-[0px_0px_10px_4px_rgba(0,0,0,0.25)] p-1 sm:p-5 flex flex-col items-center transition-all gap-5"
+            className={clsx("rounded-2xl p-1 sm:p-5 flex flex-col items-center transition-all gap-5",
+              isDark ? "hover:shadow-[0px_0px_10px_4px_rgba(255,255,255,0.25)]" : "hover:shadow-[0px_0px_10px_4px_rgba(0,0,0,0.25)]"
+            )}
           >
             <Image 
               src={Pikachu} 
@@ -39,7 +48,9 @@ export default function Home(){
           </Link>
 
           <Link href={'/crypto-dashboard'}
-            className="rounded-2xl hover:shadow-[0px_0px_10px_4px_rgba(0,0,0,0.25)] p-1 sm:p-5 flex flex-col items-center transition-all gap-5"
+            className={clsx("rounded-2xl p-1 sm:p-5 flex flex-col items-center transition-all gap-5",
+              isDark ? "hover:shadow-[0px_0px_10px_4px_rgba(255,255,255,0.25)]" : "hover:shadow-[0px_0px_10px_4px_rgba(0,0,0,0.25)]"
+            )}
           >
             <Image 
               src={Bitcoin} 
@@ -62,6 +73,6 @@ export default function Home(){
           </Link>
         </div>
       </main>
-    </>
+    </div>
   );
 }
--- components/header.tsx
+++ components/header.tsx
@@ -1,8 +1,15 @@
 import Link from "next/link";
+import { Dispatch, SetStateAction } from "react";
+import clsx from "clsx";
 
-export default function Header(){
+export default function Header({isDark, setIsDark}: {
+  isDark: boolean,
+  setIsDark: Dispatch<SetStateAction<boolean>>
+}){
   return(
-    <header className="flex flex-row flex-wrap items-center w-full bg-lime-300 h-20 text-center gap-5 px-5 justify-between text-lg sm:flex-nowrap sm:px-10 sm:text-2xl sm:gap-20 sm:justify-normal sm:-translate-y-15 sm:hover:translate-0 sm:transition">
+    <header className={clsx("flex flex-row flex-wrap items-center w-full h-20 text-center gap-5 px-5 justify-between text-lg sm:flex-nowrap sm:px-10 sm:text-2xl sm:gap-20 sm:justify-normal sm:-translate-y-15 sm:hover:translate-0 sm:transition",
+      isDark ? "bg-lime-700 text-white" : "bg-lime-300 text-black"
+    )}>
       <Link href={'/'} className="underline decoration-0 underline-offset-10 hover:decoration-4 transition-all duration-100">
         Home        
       </Link>
@@ -15,8 +22,14 @@ export default function Header(){
         Crypto Dashboard
       </Link>
 
-      <button className=" ml-auto hover:outline-2 px-5 py-2 rounded-xl">
-        Light Mode
+      <button className=" ml-auto hover:outline-2 px-5 py-2 rounded-xl"
+        onClick={() => {
+          setIsDark(!isDark);
+        }}
+      >
+        {
+          isDark ? "Light Mode" : "Dark Mode"
+        }
       </button>
     </header>
   );
